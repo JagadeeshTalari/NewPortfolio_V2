ed3227349df208d433f33f5d4491ee83c11dbd00
Jagadeesh Talari <talarijagadeesh55@gmail.com>
2024-02-07 22:24:26 +0530

    About section updated v1

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -4,9 +4,8 @@ import WorkHistory from './WorkHistory'
 const About = () => {
   return (
     <div className="about">
-
-    <div>About</div>
-    <p>Innovative professional with a 3 year experience in developing (full stack), backend / frontend, is accredited by peers, subordinates, senior leaders as well as clients for tenacity and the ability to gracefully handle the dynamics of a rapidly changing corporate environment.</p>
+    <div className='sectionHeading'>ABOUT</div>
+    <p>Innovative professional with a 2 year experience in developing (full stack), backend / frontend, is accredited by peers, subordinates, senior leaders as well as clients for tenacity and the ability to gracefully handle the dynamics of a rapidly changing corporate environment.</p>
     <WorkHistory/>
     </div>
   )
--- src/Components/About/WorkHistory.jsx
+++ src/Components/About/WorkHistory.jsx
@@ -5,33 +5,33 @@ import Circles from './circles'
 const WorkHistory = () => {
   return (
     <div id='workHistory'>
-        <h4>WorkHistory</h4>
+        <h4 className='sectionHeading'>WORK HISTORY</h4>
         <div className="workHistorLine">
           <Circles activeClass="active"/>
           <div className='workDetails'>
-            <p>SchbangQ</p>
-            <p>Backend Developer (Full Time)</p>
-            <p>Mar 2023 - present</p>
+            <p className='companyName'>Tata Consultancy Services</p>
+            <p className='role'>Frontend Developer (Full Time)</p>
+            <p className='duration'>Nov 2021 - Oct 2023</p>
           </div>
           <Circles/>
           <div className='workDetails'>
-            <p>SchbangQ</p>
-            <p>Backend Developer (Full Time)</p>
-            <p>Mar 2023 - present</p>
+            <p className='companyName'>Chandu-Soft Technologies Pvt Ltd.</p>
+            <p className='role'>Junior Engineer (Full Time)</p>
+            <p className='duration'>Apr 2021 - Aug 2021</p>
           </div>
           <Circles/>
           <div className='workDetails third'>
+            <p className='companyName'>SoftTech Technologies</p>
+            <p className='role'>Junior Faculty (Full Time)</p>
+            <p className='duration'>Oct 2020 - Feb 2021</p>
+          </div>
+          {/* <Circles/> */}
+          {/* <div className='workDetails third'>
             <p>SchbangQ</p>
             <p>Backend Developer (Full Time)</p>
             <p>Mar 2023 - present</p>
           </div>
-          <Circles/>
-          <div className='workDetails third'>
-            <p>SchbangQ</p>
-            <p>Backend Developer (Full Time)</p>
-            <p>Mar 2023 - present</p>
-          </div>
-          <Circles/>
+          <Circles/> */}
         </div>
     </div>
   )
