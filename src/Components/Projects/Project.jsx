8a779419523d88140f487b6239d6059f77c8b6bd
Jagadeesh Talari <talarijagadeesh55@gmail.com>
2024-02-10 08:51:05 +0530

    Book My Show Project links added temporarily

--- src/Components/Projects/Project.jsx
+++ src/Components/Projects/Project.jsx
@@ -1,40 +1,62 @@
-import React from 'react'
-import "./Projects.css"
+import React from "react";
+import "./Projects.css";
 
-import explore from "../../assets/explore.svg"
-import project1 from "../../assets/project1.png"
-
+import explore from "../../assets/explore.svg";
+// import project1 from "../../assets/project1.png";
+import project1 from "../../assets/BMS_Logo.png";
 
 const Project = () => {
   return (
     <div>
-        <div className="project article-box">
-            <div className="projectHeading d-flex">
-                <img className='projectImg' src={project1} alt="" />
-                <div>
-                    <div className="projectName">Book My Show</div>
-                    <a href="" className="projectLink">Project Link</a>
-                    <a href="" className="sourceCode"><b>Source Code</b></a>
-                </div>
-            </div>
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatibus aspernatur velit quae impedit assumenda labore nostrum aut, optio nulla sint nesciunt quisquam molestiae at, corporis possimus ipsam veritatis temporibus tempore, consequatur culpa fugiat est officia expedita quas? Eligendi pariatur suscipit reiciendis quod exercitationem illo quos sed! Velit consequatur vero culpa.</p>
-            <div className='d-flex badgesSection'>
-              <div className="badge d-flex">
-                <div className='dot activeColor'></div>
-                Status: live
-              </div>
-              <div className="badge">Status: live</div>
-              <div className="badge">NodeJs</div>
-              <div className="badge">JavaScript</div>
-              <div className="badge">ExpressJs</div>
-              <div className="badge">Docker</div>
-              <div className="badge">MongoDB</div>
-              <div className="badge">Ubuntu</div>
-            </div>
-            <img className='explore' style={{height: "24px", opacity: "0.1"}} src={explore} alt="explore"  />
+      <div className="project article-box">
+        <div className="projectHeading d-flex">
+          <img className="projectImg" src={project1} alt="" />
+          <div>
+            <div className="projectName">Book My Show</div>
+            <a
+              href="https://book-my-show-by-jagadeesh.vercel.app/"
+              className="projectLink"
+              target="_blank"
+            >
+              Project Link
+            </a>
+            <a
+              href="https://github.com/JagadeeshTalari/BookMyShow_Project"
+              className="sourceCode"
+              target="_blank"
+            >
+              <b>Source Code</b>
+            </a>
+          </div>
         </div>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatibus
+          aspernatur velit quae impedit assumenda labore nostrum aut, optio
+          nulla sint nesciunt quisquam molestiae at, corporis possimus ipsam
+          veritatis temporibus tempore, consequatur culpa fugiat est officia
+          expedita quas? Eligendi pariatur suscipit reiciendis quod
+          exercitationem illo quos sed! Velit consequatur vero culpa.
+        </p>
+        <div className="d-flex badgesSection">
+          <div className="badge d-flex">
+            <div className="dot activeColor"></div>
+            Status: live
+          </div>
+          <div className="badge">ReactJs</div>
+          <div className="badge">JavaScript</div>
+          <div className="badge">NodeJs</div>
+          <div className="badge">ExpressJs</div>
+          <div className="badge">MongoDB</div>
+        </div>
+        <img
+          className="explore"
+          style={{ height: "24px", opacity: "0.1" }}
+          src={explore}
+          alt="explore"
+        />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Project
+export default Project;
