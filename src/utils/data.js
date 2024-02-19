9877928c48604828b283d399231de2907f0f7b30
Jagadeesh Talari <talarijagadeesh55@gmail.com>
2024-02-19 14:32:25 +0530

    movie review app updated as project v1

--- src/utils/data.js
+++ src/utils/data.js
@@ -1,52 +1,39 @@
 export const AllprojectDetails = [
-    {
-        projectName: "Unsplash Images",
-        imgLink: "https://logowik.com/content/uploads/images/unsplash8609.jpg",
-        projectLink: "https://unsplash-images-project-bay.vercel.app/",
-        sourceCodeLink: "https://github.com/JagadeeshTalari/Unsplash-Images-Project",
-        desc: "Built an application using Unsplash API in React.",
-        techUsed: [
-
-"ReactJs",
-"JavaScript",
-"React Query",
-"Axios",
-"Unsplash API",
-        ]
-    },
-    {
-        projectName: "Book My Show Clone",
-        imgLink: "https://th.bing.com/th/id/OIP.UZ0uPe5-OYGEUwi3kWPn7QHaFX?rs=1&pid=ImgDetMain",
-        projectLink: "https://book-my-show-by-jagadeesh.vercel.app/",
-        sourceCodeLink: "https://github.com/JagadeeshTalari/BookMyShow_Project",
-        desc:
-        `Developed and maintained user interface using React.js resulting in reduction in development time for the project. Utilized Redux for state management, resulting in reduction in code complexity and increase productivity.`,
-        techUsed: [
-"ReactJs",
-"JavaScript",
-"Redux",
-"ReduxToolKit",
-"BootStrap",
-        ]
-    },
-
-   
-]
+  {
+    projectName: "Movie Review App",
+    imgLink:
+      "https://th.bing.com/th/id/OIP.UZ0uPe5-OYGEUwi3kWPn7QHaFX?rs=1&pid=ImgDetMain",
+    projectLink: "https://movies-review-app-jagadeesh.vercel.app/",
+    sourceCodeLink: "https://github.com/JagadeeshTalari/movix",
+    desc: `Developed and maintained user interface using React.js resulting in reduction in development time for the project. Utilized Redux for state management, resulting in reduction in code complexity and increase productivity.`,
+    techUsed: ["ReactJs", "JavaScript", "Redux", "ReduxToolKit", "BootStrap"],
+  },
+  {
+    projectName: "Unsplash Images",
+    imgLink: "https://logowik.com/content/uploads/images/unsplash8609.jpg",
+    projectLink: "https://unsplash-images-project-bay.vercel.app/",
+    sourceCodeLink:
+      "https://github.com/JagadeeshTalari/Unsplash-Images-Project",
+    desc: "Built an application using Unsplash API in React.",
+    techUsed: ["ReactJs", "JavaScript", "React Query", "Axios", "Unsplash API"],
+  },
+];
 
 export const AllCertificateDetails = [
-    {
-        name:"Git & GitHub",
-        credentialUrl:"https://s3-ap-southeast-1.amazonaws.com/learnyst/schools/2410/certificates/24027/5738231_24027.pdf?1665287952",
-        platform:"Learn Code Online",
-        duration:"09th Oct 2022 - Doesn't expire",
-        imgLink: "./assets/git_github_certificate.png"
-    },
-    {
-        name:"HTML & CSS",
-        credentialUrl:"https://s3-ap-southeast-1.amazonaws.com/learnyst/schools/2410/certificates/98072/5738231_98072.pdf?1659974727",
-        platform:"Learn Code Online",
-        duration:"08th Aug 2022 - Doesn't expire",
-        imgLink: "./assets/html_css_certificate.png"
-    },
-
-]
+  {
+    name: "Git & GitHub",
+    credentialUrl:
+      "https://s3-ap-southeast-1.amazonaws.com/learnyst/schools/2410/certificates/24027/5738231_24027.pdf?1665287952",
+    platform: "Learn Code Online",
+    duration: "09th Oct 2022 - Doesn't expire",
+    imgLink: "./assets/git_github_certificate.png",
+  },
+  {
+    name: "HTML & CSS",
+    credentialUrl:
+      "https://s3-ap-southeast-1.amazonaws.com/learnyst/schools/2410/certificates/98072/5738231_98072.pdf?1659974727",
+    platform: "Learn Code Online",
+    duration: "08th Aug 2022 - Doesn't expire",
+    imgLink: "./assets/html_css_certificate.png",
+  },
+];
