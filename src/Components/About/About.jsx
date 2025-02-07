abdd71ea031b5761f2bb2053795deca2452b098c
Jagadeesh Talari <talarijagadeesh55@gmail.com>
2025-02-07 20:35:37 +0530

    updated  about and workhistory

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,14 +1,22 @@
-import React from 'react'
-import WorkHistory from './WorkHistory'
+import React from "react";
+import WorkHistory from "./WorkHistory";
 
 const About = () => {
   return (
     <div className="about">
-    <div className='sectionHeading'>ABOUT</div>
-    <p>Innovative professional with a 2 year experience in developing (full stack), backend / frontend, is accredited by peers, subordinates, senior leaders as well as clients for tenacity and the ability to gracefully handle the dynamics of a rapidly changing corporate environment.</p>
-    <WorkHistory/>
+      <div className="sectionHeading">ABOUT</div>
+      <p>
+        Dedicated Full-Stack Developer with 3 years of experience in React.js,
+        Material-UI, and Tailwind CSS, specializing in building intuitive and
+        responsive user interfaces. Experienced in delivering large-scale
+        projects for the pharma industry, including access management
+        dashboards. Proficient in full-stack development, integrating RESTful
+        APIs with Node.js and Python, and leveraging DevOps tools like Docker,
+        Azure, and Git to deliver scalable and efficient solutions.
+      </p>
+      <WorkHistory />
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
--- src/Components/About/WorkHistory.jsx
+++ src/Components/About/WorkHistory.jsx
@@ -1,40 +1,46 @@
-import React from 'react'
-import "./WorkHistory.css"
-import Circles from './Circles'
+import React from "react";
+import "./WorkHistory.css";
+import Circles from "./Circles";
 
 const WorkHistory = () => {
   return (
-    <div id='workHistory'>
-        <h4 className='sectionHeading'>WORK HISTORY</h4>
-        <div className="workHistorLine">
-          <Circles activeClass="active"/>
-          <div className='workDetails'>
-            <p className='companyName'>Tata Consultancy Services</p>
-            <p className='role'>Frontend Developer (Full Time)</p>
-            <p className='duration'>Nov 2021 - Oct 2023</p>
-          </div>
-          <Circles/>
-          <div className='workDetails'>
-            <p className='companyName'>Chandu-Soft Technologies Pvt Ltd.</p>
-            <p className='role'>Junior Engineer (Full Time)</p>
-            <p className='duration'>Apr 2021 - Aug 2021</p>
-          </div>
-          <Circles/>
-          <div className='workDetails third'>
-            <p className='companyName'>SoftTech Technologies</p>
-            <p className='role'>Junior Faculty (Full Time)</p>
-            <p className='duration'>Oct 2020 - Feb 2021</p>
-          </div>
-          {/* <Circles/> */}
-          {/* <div className='workDetails third'>
+    <div id="workHistory">
+      <h4 className="sectionHeading">WORK HISTORY</h4>
+      <div className="workHistorLine">
+        <Circles activeClass="active" />
+        <div className="workDetails">
+          <p className="companyName">Aapmor Technologies</p>
+          <p className="role">SDE II (Full Time)</p>
+          <p className="duration">Feb 2024 - present</p>
+        </div>
+        <Circles />
+        <div className="workDetails">
+          <p className="companyName">Tata Consultancy Services</p>
+          <p className="role">Frontend Developer (Full Time)</p>
+          <p className="duration">Nov 2021 - Oct 2023</p>
+        </div>
+        {/* <Circles />
+        <div className="workDetails">
+          <p className="companyName">Chandu-Soft Technologies Pvt Ltd.</p>
+          <p className="role">Junior Engineer (Full Time)</p>
+          <p className="duration">Apr 2021 - July 2021</p>
+        </div> */}
+        {/* <Circles /> */}
+        {/* <div className="workDetails third">
+          <p className="companyName">SoftTech Technologies</p>
+          <p className="role">Junior Faculty (Full Time)</p>
+          <p className="duration">Oct 2020 - Feb 2021</p>
+        </div> */}
+        {/* <Circles/> */}
+        {/* <div className='workDetails third'>
             <p>SchbangQ</p>
             <p>Backend Developer (Full Time)</p>
             <p>Mar 2023 - present</p>
           </div>
           <Circles/> */}
-        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default WorkHistory
+export default WorkHistory;
